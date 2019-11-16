af0fe23391e81217ad3d384cf7fc495dea9ce06e
Lilia Evgeniou <liliaevgeniou@gmail.com>
2019-11-16 15:41:57 +0000

    skeleton

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className={"App"}>
+
+        <div className={"Title"}>
+          Wearther
+        </div>
+
+        <div className={"Weather"}>
+          10ºC
+        </div>
+
+        <div className={"Clothes"}>
+          <div className={"Suggestions"}>
+
+            <div className={"TopsSuggestions"}>
+                tank top, tshirt, jacket
+            </div>
+            <div className={"BottomsSuggestions"}>
+                dress, tights
+            </div>
+
+            <button className={"ButtonEditSuggestion"}>
+
+            </button>
+          </div>
+
+        </div>
+
+        <div className={"TemperatureBar"}>
+
+        </div>
+        <button className={"ButtonHistory"}>
+
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
